11cca309a90b8f7b45e9195fb41442cf87b89d45
Ole-Lewi <lewisino666@gmail.com>
2024-10-26 10:37:08 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 USE alx_book_store;
 
 INSERT INTO Customers(customer_id, customer_name,email,address)
@@ -6,10 +6,4 @@ VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.')
     (3, 'Obed Ehoneah', 'eobed@sandtech.com','125 Happiness Ave.')
     (4,'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness Ave.')
-mysql -u root -p alx_book_store >task_6.sql
-=======
-INSERT INTO customer(customer_id, customer_name,email,address)
-VALUES(2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave.")
-    
-   
->>>>>>> 3972a98162480fc70708c9aaee85617ffd2b6be4
+mysql -u root -p alx_book_store >task_6.sql;
